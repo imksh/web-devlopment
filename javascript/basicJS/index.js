6b35d0739d7c02b3c8b2b7e62454b8e2c55a2cf4
Karan Sharma <karan03945@gmail.com>
2025-11-17 17:40:21 +0530

    bootstrap assignments added

---
+++ javascript/basicJS/index.js
@@ -0,0 +1,20 @@
+var a = 10;
+var a = 20;
+
+let b = 10;
+
+console.log(a, b);
+
+let arr = [1, 2, "abc", "a"];
+console.log(arr);
+
+console.warn("warning");
+console.error("error");
+
+//alert("hello developer")
+
+if (confirm("hello")) {
+  console.log("thanks");
+} else {
+  console.log("its ok");
+}
